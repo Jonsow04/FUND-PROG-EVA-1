d97d2741d6b428abedbe5858b7f8105c56f44c3a
Jonsow04 <143568790+Jonsow04@users.noreply.github.com>
2023-09-12 19:41:48 -0600

    Add files via upload

---
+++ EVA1_9_AREA_TRIANGULO/src/eva1_9_area_triangulo/EVA1_9_AREA_TRIANGULO.java
@@ -0,0 +1,23 @@
+
+package eva1_9_area_triangulo;
+import java.util.*;
+import java.lang.Math;
+
+public class EVA1_9_AREA_TRIANGULO {
+    private static Scanner input = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        //Declaración de variables
+        double base;
+        double altura;
+        double area;
+        
+        System.out.println("Introduzca la base del triángulo:");
+        base = input.nextDouble();
+        System.out.println("Introduzca la altura del triángulo:");
+        altura = input.nextDouble();
+        area = (base * altura) / 2;
+        System.out.println("El área del triángulo es: " + area);
+    }
+    
+}
