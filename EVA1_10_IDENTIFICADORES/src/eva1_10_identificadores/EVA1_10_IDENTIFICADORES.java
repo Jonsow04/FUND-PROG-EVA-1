bd7785a65e0d11f9cdca32dfc0f7fa114a725531
Jonsow04 <143568790+Jonsow04@users.noreply.github.com>
2023-09-13 19:44:18 -0600

    Add files via upload

---
+++ EVA1_10_IDENTIFICADORES/src/eva1_10_identificadores/EVA1_10_IDENTIFICADORES.java
@@ -0,0 +1,25 @@
+
+package eva1_10_identificadores;
+
+public class EVA1_10_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //int 1salario; no cumple con las reglas de identificadores, incia con un número
+        //int sal&rio; nom cumple: & no es un caracter alfanumérico
+        int mi_salario;
+        int $salario;
+        int salario1000;
+        //int _; no es válido
+        int $;
+        //int mi salario; no válido, espacio en blanco
+        
+        int salario;
+        int Salario;
+        int SALARIO;
+        //int salario; no podemos repetir identificadores
+        
+        //int public; no podemos usar keywords (palabras reservadas)
+        
+    }
+    
+}
