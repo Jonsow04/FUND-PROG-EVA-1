3361cde0b49225033d38cde82e0bb394e192dd67
Jonsow04 <143568790+Jonsow04@users.noreply.github.com>
2023-09-27 23:27:58 -0600

    Add files via upload

---
+++ EVA1_11_INICIALIZACION/src/eva1_11_inicializacion/EVA1_11_INICIALIZACION.java
@@ -0,0 +1,25 @@
+
+package eva1_11_inicializacion;
+
+public class EVA1_11_INICIALIZACION {
+
+    public static void main(String[] args) {
+        //Los paréntesis denotan una función:
+        //println() = función
+        //main() = función
+        System.out.println("Hola mundo");
+        int valor = 0; //Inicializar la variable para usarla
+        System.out.println(valor);
+        
+        //Declaramos e inicializamos 3 variables double
+        double salario = 0, iva = 0.16, subtotañ = 10000;
+        
+        final int califa_pasar = 70;
+        final String ESCUELA = "Instituto Tecnológico de Chihuahua II";
+        
+        //Error: solo se puede asignar una vez
+        //califa_pasar = 100;
+        //califa_pasar = 20;
+    }
+    
+}
